aecf09e1d66900e37b85042539b732522284e512
aakashhh-17 <aakashbhujbal1704@gmail.com>
2025-07-08 11:54:40 +0530

    tailwind setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import Movies from './pages/Movies'
 import MovieDetails from './pages/MovieDetails'
 import SeatLayout from './pages/SeatLayout'
 import MyBookings from './pages/MyBookings'
 import Favourite from './pages/Favourite'
+import {Toaster} from 'react-hot-toast'
+import Footer from './components/Footer'
 
 const App = () => {
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
   return (
     <>
-      <Navbar />
+    <Toaster />
+     { !isAdminRoute && <Navbar />}
       <Routes >
         <Route path='/' element={<Home />}/>
         <Route path='/movies' element={<Movies />}/>
@@ -20,6 +24,7 @@ const App = () => {
         <Route path='/my-bookings' element={<MyBookings />}/>
         <Route path='/favourite' element={<Favourite />}/>
       </Routes>
+      {!isAdminRoute && <Footer />}
     </>
   )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className=''>
+        <h1 className='text-2xl w-full mx-auto'>Home</h1>
+    </div>
   )
 }
 
