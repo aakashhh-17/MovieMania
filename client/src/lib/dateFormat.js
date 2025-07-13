a122437f035cd48f33321636dd6a8c6ecf43c5f1
aakashhh-17 <aakashbhujbal1704@gmail.com>
2025-07-13 16:36:38 +0530

    completed Bookings page UI

---
+++ client/src/lib/dateFormat.js
@@ -0,0 +1,9 @@
+export const dateFormat = (dateTime)=>{
+return new Date(dateTime).toLocaleString("en-US", {
+    weekday: 'short',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
+})
+}
--- client/src/lib/timeFormat.js
+++ client/src/lib/timeFormat.js
@@ -3,3 +3,13 @@ const hours = Math.round(time/60);
 const minutes = time % 60;
 return `${hours}h ${minutes}m`;
 }
+
+export const isoTimeFormat = (dateTime)=>{
+    const date = new Date(dateTime);
+    const localTime = date.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        hour12: true
+    });
+    return localTime;
+}
