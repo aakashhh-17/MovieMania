177106a28d2871d2886d8ddfce7fbce1aa53bb60
aakashhh-17 <aakashbhujbal1704@gmail.com>
2025-12-02 19:49:34 +0530

    inngest fix

--- server/server.js
+++ server/server.js
@@ -3,16 +3,22 @@ import cors from "cors"
 import dotenv from 'dotenv'
 import connectDB from "./configs/db.js";
 dotenv.config();
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 const app = express();
 const port = process.env.PORT;
 
 await connectDB();
+
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use(clerkMiddleware())
 
 app.get('/', (req, res)=>res.send('Server is live!'));
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
